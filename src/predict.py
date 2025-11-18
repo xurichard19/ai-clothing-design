199c6f45d661b50305cd9b3174610fd64a717109
xurichard19 <xurichard19@gmail.com>
2025-11-18 15:46:28 -0500

    training correction

---
+++ src/predict.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    pass
+
+# generate preds for sample images
+
+if __name__ == "__main__":
+    main()
--- src/train.py
+++ src/train.py
@@ -2,7 +2,8 @@ import os
 import csv
 import file_operations
 import torch
-from transformers import CLIPProcessor, CLIPModel, AdamW, get_scheduler
+from transformers import CLIPProcessor, CLIPModel, get_scheduler
+from PIL import Image
 
 def main():
     # generate split files if not available
@@ -17,22 +18,35 @@ def main():
 
     model, processor = load_model()
 
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True, collate_fn=lambda examples: collate(examples, processor))
-    val_loader = torch.utils.data.DataLoader(val_data, batch_size=32, shuffle=False, collate_fn=lambda examples: collate(examples, processor))
-    
-    NUM_EPOCHS = 20
+    NUM_EPOCHS = 10
+    BATCH_SIZE = 50
+
+    train_loader = torch.utils.data.DataLoader(
+        train_data, 
+        batch_size=BATCH_SIZE, 
+        shuffle=True, 
+        collate_fn=lambda examples: collate(examples, processor)
+        )
+    val_loader = torch.utils.data.DataLoader(
+        val_data, 
+        batch_size=BATCH_SIZE, 
+        collate_fn=lambda examples: collate(examples, processor)
+        )
+
     optimizer, lr_scheduler = setup_optimizer(train_loader, model, NUM_EPOCHS)
 
+    # for computing cluster
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"using {device}")
     model.to(device)
 
-    model.train()
     for epoch in range(NUM_EPOCHS):
-        total_train_loss = 0
+        model.train()
+        total_train_loss = 0.0
+
         for batch in train_loader:
             batch = {k: v.to(device) for k,v in batch.items()}
-            outputs = model(**batch)
+            outputs = model(**batch, return_loss=True)
             loss = outputs.loss
             loss.backward()
             optimizer.step()
@@ -40,15 +54,17 @@ def main():
             optimizer.zero_grad()
             total_train_loss += loss.item()
         avg_train_loss = total_train_loss / len(train_loader)
+
         model.eval()
         total_val_loss = 0
         with torch.no_grad():
             for batch in val_loader:
-                pass
-            # finish validation step
+                batch = {k: v.to(device) for k,v in batch.items()}
+                outputs = model(**batch, return_loss=True)
+                total_val_loss += outputs.loss.item()
+        avg_val_loss = total_val_loss / len(val_loader)
+        print(f'epoch {epoch} with train loss {avg_train_loss} and val loss {avg_val_loss}')
 
-        print() # PRINT ACCURACY
-    
     model.save_pretrained("./models/")
     processor.save_pretrained("./models/processors/")
     
@@ -59,7 +75,7 @@ def load_csv(split: str) -> list[dict]:
         data = [{"image": row["image"], "caption": row["caption"]} for row in reader]
     return data
 
-def load_model(model_name="openai/clip-vit-base-patch32"):
+def load_model(model_name="openai/clip-vit-base-patch32") -> list[CLIPModel, CLIPProcessor]:
     # load model and processor
     model = CLIPModel.from_pretrained(model_name)
     processor = CLIPProcessor.from_pretrained(model_name)
@@ -72,8 +88,7 @@ def load_model(model_name="openai/clip-vit-base-patch32"):
     return model, processor
 
 def setup_optimizer(train_loader: torch.utils.data.DataLoader, model: CLIPModel, epochs: int):
-    optimizer = torch.optim.Adam() # USE ADAM INSTEAD OF ADAMW
-    optimizer = AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-6)
+    optimizer = torch.optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-6)
     training_steps = epochs * len(train_loader)
     lr_scheduler = get_scheduler(
         "cosine",
@@ -85,7 +100,7 @@ def setup_optimizer(train_loader: torch.utils.data.DataLoader, model: CLIPModel,
 
 def collate(examples, processor: CLIPProcessor):
     texts = [example["caption"] for example in examples]
-    images = [example["image"] for example in examples]
+    images = [Image.open('./data/df-training-data/' + example["image"]) for example in examples]
     return processor(images, texts, return_tensors="pt", padding=True, truncation=True)
 
 if __name__ == "__main__":
