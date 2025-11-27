a8c37fc59947e6a0e7237628e7f6a797e112d362
xurichard19 <xurichard19@gmail.com>
2025-11-27 16:52:49 -0500

    predict overhaul and plan

--- src/predict.py
+++ src/predict.py
@@ -1,10 +1,14 @@
+import base64
 from dotenv import load_dotenv
 import json
+import numpy as np
 import os
 from openai import OpenAI
 from PIL import Image
+from sklearn.cluster import KMeans
 import torch
 from transformers import CLIPProcessor, CLIPModel
+from tqdm import tqdm
 
 def main():
     load_dotenv()
@@ -21,36 +25,55 @@ def main():
 
     results = {}
     samples = [sample for sample in os.listdir("./data/custom-data") if sample != ".DS_Store"]
-    print(f"predicting {len(samples)} images...")
+    print(f"\npredicting {len(samples)} images...\n")
 
-    for sample in samples:
+    predict_loop = tqdm(samples, desc="prediction")
+    for sample in predict_loop:
         tensor = get_tensor(model, processor, device, "./data/custom-data/" + sample)
         results[sample] = tensor
+
+    # find kmeans clusters to avoid sending length json queries
+    tensors = np.array(list(results.values()))
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(tensors)
+    centroids = kmeans.cluster_centers_
+    # normalize
+    centroids /= np.linalg.norm(centroids, keepdims=True)
+    cluster_results = centroids.tolist()
+    print(f"\ncompleted kmeans clustering on {len(results)} samples\n")
+
+    # IMPLEMENT:
+    # RUN get_text_features ON EVERY TEXT IN CAPTION DATASET
+    # STORE DICT OF FEATURES
+    # COMPUTE COSINE SIMILARITY ON LARGEST CLUSTER AND CAPTION
+    # RETURN CAPTIONS PASSING A THRESHOLD
+
+    # generate tensors and base64 for sample image to give baseline comparison NO NEED
+    sample_image = 0 #placeholder
+    sample_tensor = get_tensor(model, processor, device, "./data/sample.png")
+
+    # compile into single json input
+    input = {
+        "clusters": cluster_results,
+        "sample image": {
+            "tensor": sample_tensor,
+            "image description": sample_image
+        }
+    }
     
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     response = client.responses.create(
-        model="gpt-4o-mini",
+        model="gpt-5-mini",
         input=[
             {
                 "role": "system",
                 "content": (
-                    "You are a fashion analyst. Given CLIP embeddings of some sample fashion "
-                    "images, identify patterns like silhouettes, textures, fits, colors, and "
-                    "overall themes. You may use fashion specific terms including but not limited to "
-                    "deconstruction, trompe de l'oeil, futurism, relaxed fit, etc. to understand the trends. "
-                    
-                    "Then, generate a prompt for a diffusion model to generate a clothing "
-                    "design based on the observed trends in the sample fashion images. For "
-                    "example, you could say something akin to 'Generate a clothing design for "
-                    "a pair of denim jeans with a baggy silhouette and a vintage theme that "
-                    "combines streetwear and avant-garde aesthetics through a distressed finish "
-                    "and a mud wash'. You should describe key features of the new design and "
-                    "the overall theme of the piece."
+                    ""
                 )
             },
             {
                 "role": "user",
-                "content": json.dumps({"image_embeddings": results})
+                "content": json.dumps(input)
             }
         ]
     )
