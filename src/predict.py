dd2159c3976c095e7f36e3580e78cabdd875c537
xurichard19 <xurichard19@gmail.com>
2025-11-28 03:11:42 -0500

    implementing cosine similarity

--- src/predict.py
+++ src/predict.py
@@ -1,4 +1,3 @@
-import base64
 from dotenv import load_dotenv
 import json
 import numpy as np
@@ -9,6 +8,8 @@ from sklearn.cluster import KMeans
 import torch
 from transformers import CLIPProcessor, CLIPModel
 from tqdm import tqdm
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 def main():
     load_dotenv()
@@ -32,38 +33,25 @@ def main():
         tensor = get_tensor(model, processor, device, "./data/custom-data/" + sample)
         results[sample] = tensor
 
-    # find kmeans clusters to avoid sending length json queries
+    # find kmeans clusters for trend analysis
     tensors = np.array(list(results.values()))
     kmeans = KMeans(n_clusters=5)
-    kmeans.fit(tensors)
+    labels = kmeans.fit_predict(tensors)
     centroids = kmeans.cluster_centers_
+
     # normalize
-    centroids /= np.linalg.norm(centroids, keepdims=True)
-    cluster_results = centroids.tolist()
-    print(f"\ncompleted kmeans clustering on {len(results)} samples\n")
+    centroids /= np.linalg.norm(centroids, axis=1, keepdims=True)
 
-    # IMPLEMENT:
-    # RUN get_text_features ON EVERY TEXT IN CAPTION DATASET
-    # STORE DICT OF FEATURES
-    # COMPUTE COSINE SIMILARITY ON LARGEST CLUSTER AND CAPTION
-    # RETURN CAPTIONS PASSING A THRESHOLD
+    # find largest cluster by size (representative)
+    unique, count = np.unique(labels, return_counts=True)
+    largest_index = unique[np.argmax(count)]
+    cluster = torch.tensor(centroids[largest_index], dtype=torch.float32).to(device)
 
-    # generate tensors and base64 for sample image to give baseline comparison NO NEED
-    sample_image = 0 #placeholder
-    sample_tensor = get_tensor(model, processor, device, "./data/sample.png")
-
-    # compile into single json input
-    input = {
-        "clusters": cluster_results,
-        "sample image": {
-            "tensor": sample_tensor,
-            "image description": sample_image
-        }
-    }
+    candidate_captions = select_top_captions(model, processor, device, cluster)
     
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     response = client.responses.create(
-        model="gpt-5-mini",
+        model="gpt-4o-mini",
         input=[
             {
                 "role": "system",
@@ -73,7 +61,7 @@ def main():
             },
             {
                 "role": "user",
-                "content": json.dumps(input)
+                "content": json.dumps(candidate_captions)
             }
         ]
     )
@@ -82,13 +70,37 @@ def main():
 
 def get_tensor(model: CLIPModel, processor: CLIPProcessor, device: torch.device, path: str) -> list[float]:
     sample = Image.open(path)
-    inputs = processor(images=sample, text=[""], return_tensors="pt", padding=True).to(device)
+    inputs = processor(images=sample, return_tensors="pt").to(device)
     with torch.no_grad():
         tensor = model.get_image_features(**inputs)
-
-    # normalize
     tensor = tensor / tensor.norm(2, -1, True)
-    return tensor.cpu().tolist()[0]
+    return tensor[0]
+
+def generate_caption_tensors(model: CLIPModel, processor: CLIPProcessor, device: torch.device, path: str) -> dict:
+    captions = {}
+
+    with open(path, "r") as file:
+        embed_captions = tqdm(file.readlines(), desc="gathering text features")
+        for caption in embed_captions:
+            caption = caption.strip()
+            input = processor(text=[caption], return_tensors="pt").to(device)
+            with torch.no_grad():
+                tensor = model.get_text_features(**input)
+            tensor = tensor / tensor.norm(2, -1, True)
+            captions[caption] = tensor[0]
+    return captions
+
+def select_top_captions(model: CLIPModel, processor: CLIPProcessor, device: torch.device, tensor: torch.Tensor, captions: dict) -> list:
+    # select top captions based on cosine similarity between tensors
+
+    candidate_captions = []
+    similarity_loop = tqdm(captions.keys(), desc = "finding cosine similarity")
+    for caption in similarity_loop:
+        similarity = torch.nn.functional.cosine_similarity(tensor.squeeze(), captions[caption].squeeze(), dim=0).item()
+        # select captions which exceed similarity threshold
+        if similarity >= 0.28: candidate_captions.append(caption)
+        print(f"{caption}: {similarity}")
+    return candidate_captions
 
 if __name__ == "__main__":
     main()
